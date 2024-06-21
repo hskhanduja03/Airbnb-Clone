bdc25eeb4011edac09e277bbfc0b13b02f913d8a
Harmeet Singh <hskhanduja03@gmail.com>
2024-06-21 18:13:25 +0530

    client env

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,7 +14,7 @@ import SinglePlacePage from "./pages/SinglePlacePage";
 import BookingsPage from "./pages/BookingsPage";
 import SingleBooking from "./pages/SingleBooking";
 
-axios.defaults.baseURL = "http://127.0.0.1:4000";
+axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL;
 axios.defaults.withCredentials = true;
 
 function App() {
